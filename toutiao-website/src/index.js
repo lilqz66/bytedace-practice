1539ba5830336e6b44741aa5a5613d330478e400
王莹 <2973745148@qq.com>
2021-02-09 17:15:04 +0800

    fix(Login组件): 简易登录

--- toutiao-website/src/index.js
+++ toutiao-website/src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App'
+
+import Login from './componets/login';
 import OfflineArticleList from './componets/offline-article-list';
 import OnlineArticleList from './componets/online-article-list';
 import ArticleDetail from './componets/article-detail';
@@ -18,7 +20,8 @@ render(
     <Route path='/' exact component={OnlineArticleList}></Route>
     <Route path='/OfflineArticleList' exact component={OfflineArticleList}></Route>
     <Route path='/ArticleDetail' exact component={ArticleDetail}></Route>
-    <Route path='/Comment' exact component={Likes}></Route>
+    <Route path='/Comment' exact component={CommentList}></Route>
+    <Route path='/Login' exact component={Login}></Route>
   </div>
   </BrowserRouter>,
   document.getElementById('root')
